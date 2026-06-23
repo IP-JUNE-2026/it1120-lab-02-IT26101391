c960df0f801c9e34e11f59b54fcfd6b97a358f67
IT26101391 <it26101391@my.sliit.lk>
2026-06-24 04:36:45 +0530

    Add Lab2Q2 answer

---
+++ Q2/IT26101391Lab2Q2.java
@@ -0,0 +1,10 @@
+public class IT26101391Lab2Q2 {
+public static void main(String[] args){
+	double length = 10;
+	double PI  = 3.14;
+	double squareperimeter = 4 * length;
+	double circlecircumference = squareperimeter;
+	double Radius = circlecircumference / (2*PI);
+	System.out.println("Radius of the circular fence:"+Radius);
+}
+}
